6a00825e2ee47635f67324f61d5f11a7aa26cc27
IJsniper <ijsniper1@gmail.com>
2026-07-31 11:25:33 +0300

    auth-dto

---
+++ crm-platform/back-end/src/modules/auth/dto/register.dto.ts
@@ -0,0 +1,26 @@
+import {
+    IsEmail,
+    IsString,
+    MinLength,
+    MaxLength,
+} from 'class-validator'
+
+export class RegisterDto {
+    @IsString()
+    @MinLength(2)
+    @MaxLength(50)
+    firstName!: string
+
+    @IsString()
+    @MinLength(2)
+    @MaxLength(50)
+    lastName!: string
+
+    @IsEmail()
+    email!: string
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(50)
+    password!: string
+}
