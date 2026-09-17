68f83bbf7d2a317bf800de11330157185e88272a
IJsniper <ijsniper1@gmail.com>
2026-09-17 21:22:12 +0300

    github+google

--- crm-platform/back-end/src/config/github.config.ts
+++ crm-platform/back-end/src/config/github.config.ts
@@ -1,7 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
+const backendUrl = 'https://crm-gr3n.onrender.com'
+
 export const githubConfig = registerAs('github', () => ({
   clientId: process.env.GITHUB_CLIENT_ID,
   clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackUrl: process.env.GITHUB_CALLBACK_URL,
+  callbackUrl: `${backendUrl}/api/auth/github/callback`,
 }));
--- crm-platform/back-end/src/config/google.config.ts
+++ crm-platform/back-end/src/config/google.config.ts
@@ -1,7 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
+const backendUrl = 'https://crm-gr3n.onrender.com'
 export const googleConfig = registerAs('google', () => ({
+
   clientId: process.env.GOOGLE_CLIENT_ID!,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-  callbackUrl: process.env.GOOGLE_CALLBACK_URL!,
+  callbackUrl: `${backendUrl}/api/auth/google/callback`,
 }));
