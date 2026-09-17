632d0f9622f6498a09fd3197b0092eea2217f908
IJsniper <ijsniper1@gmail.com>
2026-09-17 21:38:54 +0300

    github+google-v3

--- crm-platform/back-end/src/config/github.config.ts
+++ crm-platform/back-end/src/config/github.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
-const backendUrl = "https://www.crm-platform.site"
+const backendUrl = "'https://crm-gr3n.onrender.com'"
 
 export const githubConfig = registerAs('github', () => ({
   clientId: process.env.GITHUB_CLIENT_ID,
--- crm-platform/back-end/src/config/google.config.ts
+++ crm-platform/back-end/src/config/google.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
-const backendUrl = 'https://www.crm-platform.site'
+const backendUrl = 'https://crm-gr3n.onrender.com'
 export const googleConfig = registerAs('google', () => ({
 
   clientId: process.env.GOOGLE_CLIENT_ID!,
